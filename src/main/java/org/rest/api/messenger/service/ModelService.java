6440e0c45baabeeee2ccb1b2d1d1bcdd9e132d4d
Sergii703 <5121988zZ>
2017-12-21 10:13:02 +0100

    XML returning

--- src/main/java/org/rest/api/messenger/service/ModelService.java
+++ src/main/java/org/rest/api/messenger/service/ModelService.java
@@ -8,7 +8,7 @@ public class ModelService {
 
 	public List<Model> getAllModels(){
 		Model m1 = new Model(1L, "Hi", "Tim");
-		Model m2 = new Model(2L, "Hi again", "Steve");
+		Model m2 = new Model(2L, "Hi again", "Stev");
 		List<Model> list = new ArrayList<>();
 		list.add(m1);
 		list.add(m2);
