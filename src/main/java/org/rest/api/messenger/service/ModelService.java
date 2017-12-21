d5d5a3e1ee7ee1122a860339b3032253912f661b
Sergii703 <5121988zZ>
2017-12-21 10:19:56 +0100

    checking

--- src/main/java/org/rest/api/messenger/service/ModelService.java
+++ src/main/java/org/rest/api/messenger/service/ModelService.java
@@ -8,7 +8,7 @@ public class ModelService {
 
 	public List<Model> getAllModels(){
 		Model m1 = new Model(1L, "Hi", "Tim");
-		Model m2 = new Model(2L, "Hi again", "Stev");
+		Model m2 = new Model(2L, "Hi again", "Steve");
 		List<Model> list = new ArrayList<>();
 		list.add(m1);
 		list.add(m2);
