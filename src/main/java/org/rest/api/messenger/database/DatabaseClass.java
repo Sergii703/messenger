5a52d48af7a9ef6c0335afb4df133d474ddbbbcf
Sergii703 <5121988zZ>
2017-12-21 14:06:29 +0100

    added methods(update, remove, add, get)

--- src/main/java/org/rest/api/messenger/database/DatabaseClass.java
+++ src/main/java/org/rest/api/messenger/database/DatabaseClass.java
@@ -7,14 +7,14 @@ import org.rest.api.messenger.model.Model;
 import org.rest.api.messenger.model.Profile;
 
 public class DatabaseClass {
-	private static Map<Long, Model> model = new HashMap<>();
+	private static Map<Long, Model> models = new HashMap<>();
 	private static Map<Long, Profile> profiles = new HashMap<>();
 	
-	public static Map<Long, Model> getModel(){
-		return model;
+	public static Map<Long, Model> getModels(){
+		return models;
 	}
 	
-	public static Map<Long, Profile> getProfile(){
+	public static Map<Long, Profile> getProfiles(){
 		return profiles;
 	}
 }
--- src/main/java/org/rest/api/messenger/service/ModelService.java
+++ src/main/java/org/rest/api/messenger/service/ModelService.java
@@ -2,17 +2,44 @@ package org.rest.api.messenger.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import org.rest.api.messenger.database.DatabaseClass;
 import org.rest.api.messenger.model.Model;
 
 public class ModelService {
 
+	private Map<Long, Model> models = DatabaseClass.getModels();
+	
 	public List<Model> getAllModels(){
-		Model m1 = new Model(1L, "Hi", "Tim");
-		Model m2 = new Model(2L, "Hi again", "Stev");
-		List<Model> list = new ArrayList<>();
-		list.add(m1);
-		list.add(m2);
-		return list;
-		
+		return new ArrayList<Model>(models.values());
+	}
+	
+	public Model getModel(long id) {
+		return models.get(id);
+	}
+	
+	public Model addModel(Model message) {
+		message.setId(models.size()+1);
+		models.put(message.getId(), message);
+		return message;
+	}
+	
+	public Model udatemodel(Model message) {
+		if(message.getId()<=0) {
+			return null;
+		}
+		models.put(message.getId(), message);
+		return message;
+	}
+	
+	public Model removeModel(long id) {
+		return models.remove(id);
 	}
 }
+
+
+
+
+
+
