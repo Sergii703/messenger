864a858b380ab1ed0ceee0057b3a47ba548a01c5
Sergii703 <5121988zZ>
2017-12-21 15:02:43 +0100

    added JSON dependencies

--- src/main/java/org/rest/api/messenger/resources/Message.java
+++ src/main/java/org/rest/api/messenger/resources/Message.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -16,8 +17,16 @@ public class Message {
 	ModelService modelService = new ModelService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Model> getMessages() {
 		return modelService.getAllModels();
 	}	
+	
+	@GET
+	@Path("/{messagesId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Model getModel(@PathParam("messageId") long id) {
+		return modelService.getModel(id);
+	}
+	
 }
